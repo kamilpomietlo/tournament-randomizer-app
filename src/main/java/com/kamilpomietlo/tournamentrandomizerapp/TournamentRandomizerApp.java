4b5544544cbc5695ef8dd37dd9babdf9a8d95ce2
mortesto <kamilpomietlo1@gmail.com>
2021-03-30 12:32:51 +0200

    initial commit

---
+++ src/main/java/com/kamilpomietlo/tournamentrandomizerapp/TournamentRandomizerApp.java
@@ -0,0 +1,13 @@
+package com.kamilpomietlo.tournamentrandomizerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentRandomizerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournamentRandomizerApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/kamilpomietlo/tournamentrandomizerapp/TournamentRandomizerAppTests.java
@@ -0,0 +1,13 @@
+package com.kamilpomietlo.tournamentrandomizerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentRandomizerAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
